dd8cca6d8561872b1ff96d099c5601864331295d
Olegfox <1991oleg22@gmail.com>
2014-06-01 22:34:45 +0400

    mend

--- web/js/smoothZoom.js
+++ web/js/smoothZoom.js
@@ -1,19 +1,27 @@
 function smoothZoom(){
-    $('.pictureProduct img').smoothZoom({
-        width: $('.pictureProduct').width() - 40,
-        height: $('.pictureProduct').height(),
-        border_TRANSPARENCY: 20,
-        pan_BUTTONS_SHOW: "NO",
-        pan_LIMIT_BOUNDARY: "NO",
-        /******************************************
-         Enable Responsive settings below if needed.
-         Max width and height values are optional.
-         ******************************************/
-        responsive: false,
-        responsive_maintain_ratio: true,
-        max_WIDTH: '',
-        max_HEIGHT: ''
+    $('.pictureProduct img').one('load', function() {
+        $('.pictureProduct img').smoothZoom({
+            width: $('.pictureProduct').width() - 40,
+            height: $('.pictureProduct').height(),
+            border_TRANSPARENCY: 20,
+            pan_BUTTONS_SHOW: "NO",
+            pan_LIMIT_BOUNDARY: "NO",
+            /******************************************
+             Enable Responsive settings below if needed.
+             Max width and height values are optional.
+             ******************************************/
+            responsive: false,
+            responsive_maintain_ratio: true,
+            max_WIDTH: '',
+            max_HEIGHT: ''
+        });
+        $(".smooth_zoom_preloader div").hide();
+        $(".smooth_zoom_preloader div.noSel").show();
+        $(window).resize(function(){
+            $('.pictureProduct').html($('.pictureProduct img'));
+            smoothZoom();
+        });
+    }).each(function() {
+        if(this.complete) $(this).load();
     });
-    $(".smooth_zoom_preloader div").hide();
-    $(".smooth_zoom_preloader div.noSel").show();
 }
