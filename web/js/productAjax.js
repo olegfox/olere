90ecce2bd11f43d85a5fa7b22741e812ac7d7294
olegfox <1991oleg22@gmail.com>
2014-10-14 00:23:26 +0400

    появление окошка поверх другого при нажатии на продукт

--- web/js/productAjax.js
+++ web/js/productAjax.js
@@ -1,50 +1,70 @@
-$(function(){
-    $('.productWindow_overlay').click(function(){
-        productClose();
-    });
-    $('.inner_box a').click(function(){
+$(function () {
+    $('.inner_box a').click(function () {
         productShow(this);
     });
 });
 
-function productShow(object){
-    $('.productWindow_overlay').fadeIn(200);
-    $('.productWindow').on('click', function(e) {
-        if( e.target !== this )
-            return;
-        productClose();
-    });
-    $('.productWindow .productWindow_content').html('');
-    $('.productWindow').css({
-        'top' : '100%'
-    });
-    $('.productWindow').show();
+function productShow(object) {
+    var $clone_overlay = $('.productWindow_hide .productWindow_overlay').clone().appendTo('body'),
+        $clone_productWindow = $('.productWindow_hide .productWindow').clone().appendTo('body');
+
     $('body').css({
-        'overflow' : 'hidden'
+        'overflow': 'hidden'
     });
-    $('.productWindow .productWindow_content').load($(object).attr('href'), function(){
-        $('.productWindow').animate({
-            'top' : '0'
-        }, 200);
-        smoothZoom();
-        addCart();
-        $('.latestProducts a').click(function(){
-            productShow(this);
-        });
-    });
+    $clone_overlay
+        .click(function () {
+            productClose($clone_productWindow, $clone_overlay);
+        })
+        .css({
+            'z-index' : parseInt($('.productWindow_overlay:visible').last().css('z-index')) + 1
+        })
+        .fadeIn(200);
+    $clone_productWindow
+        .find('.productWindow_content')
+        .html('')
+        .load($(object).attr('href'), function () {
+            $clone_productWindow
+                .animate({
+                    'top': 0
+                }, 200)
+                .find('.close a').click(function () {
+                    productClose($clone_productWindow, $clone_overlay);
+                })
+                .end()
+                .find('.latestProducts a').click(function () {
+                    productShow(this);
+                });
+            smoothZoom();
+            addCart();
+        })
+        .end()
+        .on('click', function (e) {
+            if (e.target !== this)
+                return;
+            productClose($clone_productWindow, $clone_overlay);
+        })
+        .css({
+            'top': '100%',
+            'z-index' : parseInt($('.productWindow:visible').last().css('z-index')) + 1
+        })
+        .show();
 }
 
-function productClose(){
-    $('.productWindow').animate({
-        'top' : '100%'
+function productClose($productWindow, $overlay) {
+    $productWindow.animate({
+        'top': '100%'
     }, {
         duration: 200,
-        complete : function(){
-            $('.productWindow').hide();
-            $('body').css({
-                'overflow' : 'auto'
+        complete: function () {
+            $productWindow.remove();
+            if ($('.productWindow').length <= 1) {
+                $('body').css({
+                    'overflow': 'auto'
+                });
+            }
+            $overlay.fadeOut(200, function () {
+                $(this).remove();
             });
-            $('.productWindow_overlay').fadeOut(200);
         }
     });
 }
